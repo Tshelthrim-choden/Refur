3262e68f35b5a9637aebc8e84251c153328ca366
Tshelthrim <tshelthrimchoden678@gmail.com>
2024-02-20 22:52:11 +0800

    3

--- src/component/about/about-us.tsx
+++ src/component/about/about-us.tsx
@@ -9,7 +9,6 @@ const AboutUs: React.FC = () => {
         <p>We are team passionate about reducing waste and making a positive impact on the environment. Our journey began with a shared vision to create sustainable and stylish furniture solutions. Welcome to our eco-friendly furniture store!</p>
       </div>
       <div className="image-section">
-        {/* Add a circular image */}
         <img src="/images/cub.png" alt="About Us" className="circular-image" />
       </div>
     </section>
